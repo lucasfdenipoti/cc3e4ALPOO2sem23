55064ba78f38883f9d410cf5d4828466ecc62951
Lucas Denipoti <lucasfdenipoti@gmail.com>
2023-08-25 21:33:15 -0300

    Adição da Pesquisa

--- CRUDPessoas/src/DAL/PessoaDAO.java
+++ CRUDPessoas/src/DAL/PessoaDAO.java
@@ -28,4 +28,33 @@ public class PessoaDAO{
             conexao.desconectar();
         }
     }
+    
+    public Pessoa pesquisarPessoaPorId(Pessoa pessoa){
+        this.mensagem = "";
+        Conexao conexao = new Conexao();
+        Connection con = conexao.conectar();
+        try{
+            String comSql = "select * from pessoas "
+                    + "where idPessoas = ?";            
+            PreparedStatement stmt = con.prepareStatement(comSql);
+            stmt.setString(1, pessoa.nome);
+            stmt.executeQuery();
+            ResultSet resultset = stmt.executeQuery();
+            if(resultset.next()){
+                pessoa.nome = resultset.getString("nome");
+                pessoa.rg = resultset.getString("rg");
+                pessoa.cpf = resultset.getString("cpf");
+            }
+            else{
+                this.mensagem = "Não existe registro com este ID";
+            }
+        }
+        catch (SQLException e){
+            this.mensagem = "Erro de conexao BD";
+        }
+        finally{
+            conexao.desconectar();
+        }
+        return pessoa;
+    }
 }
