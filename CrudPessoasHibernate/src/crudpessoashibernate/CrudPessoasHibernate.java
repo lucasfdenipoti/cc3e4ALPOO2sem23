ac9a4fd9ed68f26ff404d74bac8230be89ad4de2
Lucas Denipoti <lucasfdenipoti@gmail.com>
2023-10-06 20:32:53 -0300

    criação do hibernate

--- CrudPessoasHibernate/src/crudpessoashibernate/CrudPessoasHibernate.java
+++ CrudPessoasHibernate/src/crudpessoashibernate/CrudPessoasHibernate.java
@@ -1,26 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package crudpessoashibernate;
 
 import Apresentacao.frmPrincipal;
 
-/**
- *
- * @author rever
- */
 public class CrudPessoasHibernate
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
         frmPrincipal frmP = new frmPrincipal(null, true);
         frmP.setVisible(true);
+        System.exit(0);
     }
     
 }
