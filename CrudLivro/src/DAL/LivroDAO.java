a8918e4ec9fce1d86dd3273b8c9de2b3e30fdf4b
Lucas Denipoti <lucasfdenipoti@gmail.com>
2023-10-10 21:27:16 -0300

    Pesquisar, Editar e Excluir feitos

--- CrudLivro/src/DAL/LivroDAO.java
+++ CrudLivro/src/DAL/LivroDAO.java
@@ -1,7 +1,9 @@
 package DAL;
 
 import modelo.Livro;
+import org.hibernate.Query;
 import org.hibernate.Session;
+
 
 public class LivroDAO {
     public String mensagem;
@@ -15,6 +17,59 @@ public class LivroDAO {
             session.getTransaction().commit();
             session.flush();
             this.mensagem = "Livro Cadastrado com Sucesso!";
+        } 
+        catch (Exception e) {
+            this.mensagem = "Erro de Banco de Dados";
+        }
+        finally {
+            session.close();
+        }
+    }
+    
+    public void PesquisarPorId(Livro livro){
+        this.mensagem = "";
+        try {
+            Query query = session.createQuery("from modelo.Livro l where l.idLivro = :id");
+            query.setParameter("id", livro.getIdLivro());
+            if(query.list().size() > 0){
+                livro = (modelo.Livro) query.list().get(0);
+            }
+            else
+                this.mensagem = "Registro não Encontrado";
+        } 
+        catch (Exception e) {
+            this.mensagem = "Erro de Banco de Dados";
+        }
+        finally {
+            session.close();
+        }
+    }
+    
+    public void Editar(Livro livro){
+        this.mensagem = "";
+        try {
+            session.beginTransaction();
+            session.saveOrUpdate(livro);
+            session.getTransaction().commit();
+            session.flush();
+            this.mensagem = "Livro Editado com Sucesso!";
+        } 
+        catch (Exception e) {
+            this.mensagem = "Erro de Banco de Dados";
+        }
+        finally {
+            session.close();
+        }
+    }
+    
+    public void Excluir(Livro livro){
+        this.mensagem = "";
+        try {
+            session.beginTransaction();
+            session.delete(livro);
+            session.getTransaction().commit();
+            session.flush();
+            this.mensagem = "Livro Excluido com Sucesso!";
         } 
         catch (Exception e) {
             this.mensagem = "Erro de Banco de Dados";
